928bad13f39bd5c864f740b0db472bea103a2298
Cristian Donoso <donosoc@google.com>
2019-05-25 17:52:37 -0700

    Added base input support.
    This is just the rough interface defines about how to talk about input.
    The actual hook up (how to populate the Input) is left to the window
    manager or some other responsible code.

---
+++ rothko/input/input.cc
@@ -0,0 +1,111 @@
+// Copyright 2019, Cristián Donoso.
+// This code has a BSD license. See LICENSE.
+
+#include "rothko/input/input.h"
+
+#include <cstring>
+
+#include "rothko/utils/logging.h"
+
+namespace rothko {
+
+void StartFrame(Input* input) {
+  // Move the down buttons over.
+  std::memcpy(input->down_last_frame, input->down_this_frame,
+              sizeof(input->down_this_frame));
+
+  // Actual control state.
+  input->up = false;
+  input->down = false;
+  input->left = false;
+  input->right = false;
+
+  input->prev_mouse = input->mouse;
+  input->mouse = {};
+  input->mouse_offset = {};
+}
+inline bool KeyDown(Input* input, Key key) {
+  int val = (int)key;
+  ASSERT(val < (int)Key::kLast);
+  return input->down_this_frame[val];
+}
+
+inline bool KeyDownThisFrame(Input* input, Key key) {
+  int val = (int)key;
+  ASSERT(val < (int)Key::kLast);
+  return input->down_this_frame[val] && !input->down_last_frame[val];
+}
+
+inline bool KeyUpThisFrame(Input* input, Key key) {
+  int val = (int)key;
+  ASSERT(val < (int)Key::kLast);
+  return !input->down_this_frame[val] && input->down_last_frame[val];
+}
+
+const char* ToString(Key key) {
+  switch (key) {
+    case Key::kUp: return "Up";
+    case Key::kDown: return "Down";
+    case Key::kLeft: return "Left";
+    case Key::kRight: return "Right";
+    case Key::kA: return "A";
+    case Key::kB: return "B";
+    case Key::kC: return "C";
+    case Key::kD: return "D";
+    case Key::kE: return "E";
+    case Key::kF: return "F";
+    case Key::kG: return "G";
+    case Key::kH: return "H";
+    case Key::kI: return "I";
+    case Key::kJ: return "J";
+    case Key::kK: return "K";
+    case Key::kL: return "L";
+    case Key::kM: return "M";
+    case Key::kN: return "N";
+    case Key::kEnhe: return "Enhe";
+    case Key::kO: return "O";
+    case Key::kP: return "P";
+    case Key::kQ: return "Q";
+    case Key::kR: return "R";
+    case Key::kS: return "S";
+    case Key::kT: return "T";
+    case Key::kU: return "U";
+    case Key::kV: return "V";
+    case Key::kW: return "W";
+    case Key::kX: return "X";
+    case Key::kY: return "Y";
+    case Key::kZ: return "Z";
+    case Key::k0: return "0";
+    case Key::k1: return "1";
+    case Key::k2: return "2";
+    case Key::k3: return "3";
+    case Key::k4: return "4";
+    case Key::k5: return "5";
+    case Key::k6: return "6";
+    case Key::k7: return "7";
+    case Key::k8: return "8";
+    case Key::k9: return "9";
+    case Key::kBackquote: return "`";
+    case Key::kPageUp: return "PageUp";
+    case Key::kPageDown: return "PageDown";
+    case Key::kHome: return "Home";
+    case Key::kEnd: return "End";
+    case Key::kInsert: return "Insert";
+    case Key::kDelete: return "Delete";
+    case Key::kBackspace: return "Backspace";
+    case Key::kSpace: return "Space";
+    case Key::kEnter: return "Enter";
+    case Key::kEscape: return "Escape";
+    case Key::kTab: return "Tab";
+    case Key::kCtrl: return "Ctrl";
+    case Key::kAlt: return "Alt";
+    case Key::kShift: return "Shift";
+    case Key::kSuper: return "Super";
+    case Key::kLast: return "Last";
+  }
+
+  NOT_REACHED();
+  return nullptr;
+}
+
+}  // namespace rothko
---
+++ rothko/input/input.h
@@ -0,0 +1,61 @@
+// Copyright 2019, Cristián Donoso.
+// This code has a BSD license. See LICENSE.
+
+#pragma once
+
+#include <stdint.h>
+
+#include "rothko/math/vec.h"
+#include "rothko/utils/logging.h"
+
+namespace rothko {
+
+enum class Key : uint32_t {
+  kUp, kDown, kLeft, kRight,
+  kA, kB, kC, kD, kE, kF, kG, kH, kI, kJ, kK, kL, kM, kN, kEnhe /* Ñ */, kO, kP,
+  kQ, kR, kS, kT, kU, kV, kW, kX, kY, kZ,
+  k0, k1, k2, k3, k4, k5, k6, k7, k8, k9,
+  kBackquote,
+  kPageUp, kPageDown, kHome, kEnd, kInsert, kDelete,
+  kBackspace, kSpace, kEnter, kEscape,
+  kTab, kCtrl, kAlt, kShift, kSuper /* windows key */,
+  kLast,  // Not a key, used to verify the input buffer size.
+};
+const char* ToString(Key);
+
+constexpr uint32_t kMaxKeys = 128;
+static_assert((uint32_t)Key::kLast < kMaxKeys);
+
+// Represents the input state for a frame.
+// Currently there is only support for one set of input.
+struct Input {
+  bool down_last_frame[kMaxKeys] = {};
+  bool down_this_frame[kMaxKeys] = {};
+  static_assert(sizeof(down_last_frame) == sizeof(down_this_frame));
+
+  // These are equal to down_this_frame[<ARROW KEY>] == true.
+  bool up = false;
+  bool down = false;
+  bool left = false;
+  bool right = false;
+
+  struct MouseState {
+    Int2 pos;
+    Int2 wheel;   // x: Horizontal. y: Vertical.
+
+    bool left = false;
+    bool middle = false;
+    bool right = false;
+  };
+
+  MouseState prev_mouse;
+  MouseState mouse;
+  Int2 mouse_offset;
+};
+
+void StartFrame(Input*);
+bool KeyDown(Input* input, Key key);
+bool KeyDownThisFrame(Input* input, Key key);
+bool KeyUpThisFrame(Input* input, Key key);
+
+}  // rothko
