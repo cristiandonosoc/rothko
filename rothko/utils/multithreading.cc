f4197156d57fa6987d57cef4991bc5139c1c2ab2
Cristián Donoso <cristiandonosoc@gmail.com>
2019-06-04 08:01:07 -0700

    Added lock/unlock guard.

---
+++ rothko/utils/multithreading.cc
@@ -0,0 +1,30 @@
+// Copyright 2019, Cristián Donoso.
+// This code has a BSD license. See LICENSE.
+
+#include "rothko/utils/multithreading.h"
+
+#include "rothko/utils/logging.h"
+
+namespace rothko {
+
+LockGuard::LockGuard(std::mutex* mutex) : mutex_(mutex) {
+  if (mutex_)
+    mutex_->lock();
+}
+
+LockGuard::~LockGuard() {
+  if (mutex_)
+    mutex_->unlock();
+}
+
+UnlockGuard::UnlockGuard(std::mutex* mutex) : mutex_(mutex) {
+  if (mutex_)
+    mutex_->unlock();
+}
+
+UnlockGuard::~UnlockGuard() {
+  if (mutex_)
+    mutex_->lock();
+}
+
+}  // namespace rothko
